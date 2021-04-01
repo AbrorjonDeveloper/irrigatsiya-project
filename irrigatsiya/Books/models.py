f5c797be4fbe5b05039a9da0c03c881bd2720ed4
= <aahmadov271101@gmail.com>
2021-04-01 20:13:44 +0300

    in Books

--- irrigatsiya/Books/models.py
+++ irrigatsiya/Books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Books(models.Model):
+    name = models.CharField(max_length=300)
+    book = models.FileField(blank=True, upload_to='Books')
+    link = models.URLField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(blank=False, null=False)
+
+    class Meta:
+        verbose_name_plural = 'Books'
+    
+    def save(self):
+        if self.slug is None:
+            self.slug = slugify(self.name)
+        super(Books, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
